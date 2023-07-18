3784ed4352c00896c439db8c79203c16566e6bab
jorgedennis <124327415+jorgedennis@users.noreply.github.com>
2023-07-17 22:26:47 -0700

    Add files via upload

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "f7d63338e14ed6090db0fa22e250528f"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "37f51f7420dc42e58a8d07772dda50fb"
